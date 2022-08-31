b32f7710c2acf337b5df9686bc2bde8fbb268d3a
RamonaS2 <bandeiraramona2@gmail.com>
2022-08-30 21:26:22 -0300

    req 4

--- middlewares/validadeProducts.js
+++ middlewares/validadeProducts.js
@@ -1,9 +1,8 @@
 const validadeProducts = (req, res, next) => {
   const { name } = req.body;
 
-  if (!name) {
-    return res.status(400).json({ message: '"name" is required' });
-  }
+  if (!name) res.status(400).json({ message: '"name" is required' });
+
   if (name.length < 5) {
     return res
       .status(422)
